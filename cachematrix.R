bc3d7931c2096604156877589ae96b26038c8b30
mbky <mbky@icloud.com>
2014-10-13 21:21:07 -0400

    Add function bodies

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,30 @@
 ## functions do
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+    x <- NULL
+    set <- function(initData) {
+        data <<- initData
+        x <<- NULL
+    }
+    get <- function() data
+    setSolution <- function(initSolution) x <<- initSolution
+    getSolution <- function() x
+    list(set = set, get = get,
+         setSolution = setSolution,
+         getSolution = getSolution)
 }
 
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    solution <- x$getSolution()
+    if(!is.null(solution)) {
+        message("Getting cached solution...")
+        return(solution)
+    }
+    data <- x$get()
+    solution <- solve(data, ...)
+    x$setSolution(solution)
+    solution
 }
