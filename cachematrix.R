eb87dc5ef41e4e5da673f962d00a75b76f27c548
markbutler <mark.butler2@ge.com>
2014-10-15 10:59:17 -0400

    added function-level comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,12 @@
-## These functions provide the ability to 
+## These functions provide the ability to:
 ## 1.  create a cached matrix object
 ## 2.  determine the inverse matrix given a cached matrix object
 ##
 ## The actual computation of the inverse matrix will only be performed once.
 ## After that the cached value is returned.
 
-## Write a short comment describing this function
+## This function creates a matrix and returns a set of functions
+## setting and getting the matrix and the inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
     x <- NULL
     set <- function(initData) {
@@ -20,7 +21,9 @@ makeCacheMatrix <- function(x = matrix()) {
          getSolution = getSolution)
 }
 
-## Write a short comment describing this function
+## This function returns the inverse matrix of a matrix contained
+## in a cached matrix object.  If the cached value does not exist,
+## the inverse matrix is computed and cached/returned.
 cacheSolve <- function(x, ...) {
     solution <- x$getSolution()
     if(!is.null(solution)) {
